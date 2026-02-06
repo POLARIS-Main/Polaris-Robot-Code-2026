a5d5b506f118812a33df775364cb01ec57eb3787
Austin L <austinledin28@icloud.com>
2026-02-06 16:29:51 -0600

    Refactor motor control functions and implement jump sequence in main loop

--- Polaris-Robot-Code-2026/src/main.cpp
+++ Polaris-Robot-Code-2026/src/main.cpp
@@ -1,15 +1,80 @@
 #include <Arduino.h>
 
+
+void jump();
+void motorForward(int speed);
+void motorReverse(int speed);
+void stopMotor();
+void brakeMotor();
+
+#define MOTOR_PWM_PIN 9     
+#define MOTOR_IN1_PIN 7      
+#define MOTOR_IN2_PIN 8   
+
+#define COMPRESS_SPEED 200   
+#define RELEASE_SPEED 255    
+
+
+#define COMPRESS_TIME 300  
+#define RELEASE_TIME 150     
+#define BRAKE_TIME 100       
+#define JUMP_INTERVAL 20000   
+
+unsigned long lastJumpTime = 0;
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(MOTOR_PWM_PIN, OUTPUT);
+  pinMode(MOTOR_IN1_PIN, OUTPUT);
+  pinMode(MOTOR_IN2_PIN, OUTPUT);
+  
+  stopMotor();
+  
+  delay(1000);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
 
-  delay(1000);
+  if (millis() - lastJumpTime >= JUMP_INTERVAL) {
+    jump();
+    lastJumpTime = millis();
+  }
+}
 
-  digitalWrite(LED_BUILTIN, LOW);
+void jump() {
+  motorForward(COMPRESS_SPEED);
+  delay(COMPRESS_TIME);
+  stopMotor();
+  delay(100);
+  
+  motorReverse(RELEASE_SPEED);
+  delay(RELEASE_TIME);
+  
+  brakeMotor();
+  delay(BRAKE_TIME);
+  stopMotor();
+}
 
-  delay(1000);
+
+void motorForward(int speed) {
+  digitalWrite(MOTOR_IN1_PIN, HIGH);
+  digitalWrite(MOTOR_IN2_PIN, LOW);
+  analogWrite(MOTOR_PWM_PIN, speed);
+}
+
+void motorReverse(int speed) {
+  digitalWrite(MOTOR_IN1_PIN, LOW);
+  digitalWrite(MOTOR_IN2_PIN, HIGH);
+  analogWrite(MOTOR_PWM_PIN, speed);
+}
+
+void stopMotor() {
+  digitalWrite(MOTOR_IN1_PIN, LOW);
+  digitalWrite(MOTOR_IN2_PIN, LOW);
+  analogWrite(MOTOR_PWM_PIN, 0);
+}
+
+void brakeMotor() {
+  digitalWrite(MOTOR_IN1_PIN, HIGH);
+  digitalWrite(MOTOR_IN2_PIN, HIGH);
+  analogWrite(MOTOR_PWM_PIN, 255);
 }
