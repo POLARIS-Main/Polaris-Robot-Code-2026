f63deb5bb08403ca5a9c4c1a96c10cfc0d128f4f
Austin L <austinledin28@icloud.com>
2026-02-08 17:35:51 -0600

    Refactor motor control functions to support dual motor operation and implement ramping for jumping

--- Polaris-Robot-Code-2026/src/main.cpp
+++ Polaris-Robot-Code-2026/src/main.cpp
@@ -3,32 +3,42 @@
 
 void jump();
 void motorForward(int speed);
-void motorReverse(int speed);
+void motorReverseRamp(int speed, int stepDelay);
 void stopMotor();
 void brakeMotor();
 
-#define MOTOR_PWM_PIN 9     
-#define MOTOR_IN1_PIN 7      
-#define MOTOR_IN2_PIN 8   
 
-#define COMPRESS_SPEED 200   
-#define RELEASE_SPEED 255    
+#define MOTOR_A_PWM 9
+#define MOTOR_A_IN1 7
+#define MOTOR_A_IN2 8
+
+#define MOTOR_B_PWM 10
+#define MOTOR_B_IN1 11
+#define MOTOR_B_IN2 12
+
+#define COMPRESS_SPEED 50 
+#define RELEASE_SPEED 255  
 
 
-#define COMPRESS_TIME 2000  
-#define RELEASE_TIME 150     
-#define BRAKE_TIME 100       
-#define JUMP_INTERVAL 20000   
+#define COMPRESS_TIME 2000
+#define RELEASE_TIME 150  
+#define BRAKE_TIME 100   
+#define JUMP_INTERVAL 20000 
 
 unsigned long lastJumpTime = 0;
 
 void setup() {
-  pinMode(MOTOR_PWM_PIN, OUTPUT);
-  pinMode(MOTOR_IN1_PIN, OUTPUT);
-  pinMode(MOTOR_IN2_PIN, OUTPUT);
+  analogWriteResolution(8); // Set PWM resolution to 8 bits (0-255) so it is like a standard Arduino Uno
+
+  pinMode(MOTOR_A_PWM, OUTPUT);
+  pinMode(MOTOR_A_IN1, OUTPUT);
+  pinMode(MOTOR_A_IN2, OUTPUT);
+
+  pinMode(MOTOR_B_PWM, OUTPUT);
+  pinMode(MOTOR_B_IN1, OUTPUT);
+  pinMode(MOTOR_B_IN2, OUTPUT);
   
   stopMotor();
-  
   delay(1000);
 }
 
@@ -46,7 +56,7 @@ void jump() {
   stopMotor();
   delay(100);
   
-  motorReverse(RELEASE_SPEED);
+  motorReverseRamp(RELEASE_SPEED, 10);
   delay(RELEASE_TIME);
   
   brakeMotor();
@@ -56,25 +66,45 @@ void jump() {
 
 
 void motorForward(int speed) {
-  digitalWrite(MOTOR_IN1_PIN, HIGH);
-  digitalWrite(MOTOR_IN2_PIN, LOW);
-  analogWrite(MOTOR_PWM_PIN, speed);
+  digitalWrite(MOTOR_A_IN1, HIGH);
+  digitalWrite(MOTOR_A_IN2, LOW);
+  digitalWrite(MOTOR_B_IN1, HIGH);
+  digitalWrite(MOTOR_B_IN2, LOW);
+
+  analogWrite(MOTOR_A_PWM, speed);
+  analogWrite(MOTOR_B_PWM, speed);
 }
 
-void motorReverse(int speed) {
-  digitalWrite(MOTOR_IN1_PIN, LOW);
-  digitalWrite(MOTOR_IN2_PIN, HIGH);
-  analogWrite(MOTOR_PWM_PIN, speed);
+void motorReverseRamp(int targetSpeed, int stepDelay) {
+  digitalWrite(MOTOR_A_IN1, LOW);
+  digitalWrite(MOTOR_A_IN2, HIGH);
+  digitalWrite(MOTOR_B_IN1, LOW);
+  digitalWrite(MOTOR_B_IN2, HIGH);
+
+  for (int s = 0; s <= targetSpeed; s += 5) {
+    analogWrite(MOTOR_A_PWM, s);
+    analogWrite(MOTOR_B_PWM, s);
+    delay(stepDelay);
+  }
 }
 
 void stopMotor() {
-  digitalWrite(MOTOR_IN1_PIN, LOW);
-  digitalWrite(MOTOR_IN2_PIN, LOW);
-  analogWrite(MOTOR_PWM_PIN, 0);
+  digitalWrite(MOTOR_A_IN1, LOW);
+  digitalWrite(MOTOR_A_IN2, LOW);
+  analogWrite(MOTOR_A_PWM, 0);
+
+  digitalWrite(MOTOR_B_IN1, LOW);
+  digitalWrite(MOTOR_B_IN2, LOW);
+  analogWrite(MOTOR_B_PWM, 0);
 }
 
 void brakeMotor() {
-  digitalWrite(MOTOR_IN1_PIN, HIGH);
-  digitalWrite(MOTOR_IN2_PIN, HIGH);
-  analogWrite(MOTOR_PWM_PIN, 255);
+  digitalWrite(MOTOR_A_IN1, HIGH);
+  digitalWrite(MOTOR_A_IN2, HIGH);
+  analogWrite(MOTOR_A_PWM, 255);
+
+  digitalWrite(MOTOR_B_IN1, HIGH);
+  digitalWrite(MOTOR_B_IN2, HIGH);
+  analogWrite(MOTOR_B_PWM, 255);
 }
+
